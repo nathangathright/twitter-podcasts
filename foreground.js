1bddff0306f1e6159d7a5e69bc8f8cdf1b7a9dd1
Nathan Gathright <nathan.gathright@gmail.com>
2021-11-18 16:15:33 -0600

    Working iframes

--- foreground.js
+++ foreground.js
@@ -12,12 +12,42 @@ function contains(selector, text) {
 }
 
 function getEmbeddable(link) {
-  return link.replace('spotify.com/', 'spotify.com/embed/');
+  let hostname = new URL(link).hostname;
+  switch (hostname) {
+    case "podcasts.apple.com":
+      let src = link.replace('podcasts.apple.com', 'embed.podcasts.apple.com');
+      updateIframe(src, "479px", "432px");      
+      break;
+    case "open.spotify.com":
+      let src = link.replace('open.spotify.com', 'spotify.com/embed/');
+      updateIframe(src, "480px", "232px");
+    default:
+      break;
+  }  
+}
+
+function createIframe() {
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('id', 'floating-iframe');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allowtransparency', 'true');
+  iframe.setAttribute('allow', 'encrypted-media');
+  iframe.setAttribute('width', "0px");
+  iframe.setAttribute('height', "0px");
+  iframe.setAttribute('style', "position:fixed;bottom:20px;right:20px;max-width:calc(100% - 40px)");
+  document.body.appendChild(iframe);
+}
+
+function updateIframe(src = "", width = "0px", height = "0px") {
+  var iframe = document.getElementById('floating-iframe');
+  iframe.setAttribute('src', src);
+  iframe.setAttribute('width', width);
+  iframe.setAttribute('height', height);
 }
 
 function enrich() {
-  // const podcastLinks = contains("a", "podcasts.apple.com|apple.co|open.spotify.com|spoti.fi|podcasts.google.com");
-  const podcastLinks = contains('a', "open.spotify.com/");
+  // const podcastLinks = contains("a", "podcasts.apple.com/|apple.co/|open.spotify.com/|spoti.fi/|podcasts.google.com/");
+  const podcastLinks = contains('a', "podcasts.apple.com/|open.spotify.com/");
 
   podcastLinks.map((link) => {
     link.href = (link.innerText.includes('…')) ? link.innerText.slice(0, -1) : link.innerText;
@@ -25,7 +55,7 @@ function enrich() {
     link.innerText = "▶️ Listen Now";
     link.addEventListener("click", (event) => {
       event.preventDefault();
-      console.log(getEmbeddable(event.target.href));
+      getEmbeddable(event.target.href);
     });
   });
 }
@@ -37,6 +67,7 @@ if (document.readyState === 'loading') {
 }
 
 function afterDOMLoaded() {
+  createIframe();
   setInterval(() => {
     enrich();
   }, 1000);
